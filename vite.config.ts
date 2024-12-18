dfb1b68cc99b7fdc2d9625ae1f98f1a2e8e14204
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-12-18 14:45:31 -0600

    fix: `npm test` not working

--- vite.config.ts
+++ vite.config.ts
@@ -78,6 +78,6 @@ export default defineConfig({
       dir: 'node_modules/.vitest',
     },
     environment: 'jsdom',
-    include: ['s*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
