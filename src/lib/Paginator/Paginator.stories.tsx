9c629ff4bca590c36d5c54585ec9ace8f9c5933a
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-02-15 13:54:45 -0600

    chore: Paginator story remove cruft

--- src/lib/Paginator/Paginator.stories.tsx
+++ src/lib/Paginator/Paginator.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Paginator from './Paginator';
-import styles from './Paginator.stories.module.css';
 
 const meta: Meta<typeof Paginator> = {
   component: Paginator,
@@ -21,45 +20,12 @@ export default meta;
 
 type Story = StoryObj<typeof Paginator>;
 
-export const Examples: Story = {
-  render: () => {
-    return (
-      <>
-        <Paginator
-          pages={1}
-          current={1}
-          callback={() => {}}
-          className={styles.paginator}
-        />
-        <Paginator
-          pages={2}
-          current={1}
-          callback={() => {}}
-          className={styles.paginator}
-        />
-        <Paginator
-          pages={5}
-          current={3}
-          callback={() => {}}
-          className={styles.paginator}
-        />
-        <Paginator
-          pages={20}
-          current={7}
-          callback={() => {}}
-          className={styles.paginator}
-        />
-      </>
-    );
-  },
-};
-
-export const Interactive: Story = {
+export const Default: Story = {
   args: {
-    pages: 20,
-    current: 7,
     spread: 11,
+    current: 7,
+    pages: 20,
     callback: () => {},
-    className: styles.paginator,
+    className: '',
   },
 };
